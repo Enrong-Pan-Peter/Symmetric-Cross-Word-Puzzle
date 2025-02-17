ec88fd6b07d7caedfd5ca42ab3dcb2fa3b217ed9
Enrong Pan Peter <enrongpan@outlook.com>
2025-02-17 10:40:49 -0500

    Upload Code Files
    update SymCross and Dictionary files

---
+++ src/Project/crossword/SymCross.java
@@ -0,0 +1,224 @@
+package a3.crossword;
+
+import java.util.ArrayList;
+
+import a3.dictionary.Dictionary;
+
+/**
+ * A class for creating 4x4 symmetric crossword puzzles. A symmetric crossword
+ * is a crossword where the word in row i is equal to the word in column i where
+ * i = 0, 1, 2, 3 are the indexes of the rows and columns. Words of the puzzle
+ * are always in uppercase.
+ * 
+ * <p>
+ * To use this class, one creates a new {@code SymCross} object using the
+ * no-argument constructor which initializes the crossword so that all of the
+ * words are {@code XXXX}. This creates a symmetric crossword, but not a
+ * complete crossword. A complete crossword is a symmetric crossword where all
+ * of the words are 4-letter English words contained in a {@code Dictionary}
+ * object.
+ * 
+ * <p>
+ * To create a complete crossword, one adds words to the crossword using the
+ * {@code setWord} method until the crossword is complete. {@code setWord} does
+ * not enforce the symmetry property of the crossword; it is the responsibility
+ * of the user to confirm that the crossword is symmetric and complete (perhaps
+ * with the help of the {@code isSymmetric} and {@code isComplete} methods).
+ * 
+ * <p>
+ * Alternatively, one may ask the class to create a crossword by specifying the
+ * first word of the crossword (the word in the first row) using the static
+ * method {@code makeCrossword}. This method searches a {@code Dictionary} for
+ * words that satisfy the symmetry property of the crossword. Some starting
+ * words do not lead to a complete crossword.
+ *
+ */
+public class SymCross {
+
+	/**
+	 * A static dictionary used by all instances of the class.
+	 */
+	private static Dictionary dict = new Dictionary();
+
+	/**
+	 * The list of words in this puzzle stored in row order (word in the first row
+	 * of the puzzle is the first in the list).
+	 */
+	private ArrayList<String> words;
+
+	/**
+	 * Initializes this crossword so that all four words are "XXXX".
+	 */
+	public SymCross() {
+		
+		this.words = new ArrayList<String>();
+		
+		for (int i = 0; i< 4; i++) {
+			String temp = "";
+			for (int j = 0; j < 4; j++) {
+				temp = temp + 'X';
+			}
+			this.words.add(temp);
+		}
+	}
+
+	/*
+	 * Add the required constructors and methods after this comment.
+	 */
+	
+	public SymCross(SymCross other) {
+		
+		this.words = new ArrayList<String>();
+		
+		for (int i = 0; i < 4; i++) {
+			this.words.set(i, other.words.get(i));
+		}
+	}
+	
+	
+	public void setWord(int row, String word) {
+			
+		word = word.toUpperCase();
+		
+		if (row < 0 || row > 3) {
+			throw new IllegalArgumentException("row index is not valid");
+		}
+		
+		if (word.length() != 4) {
+			throw new IllegalArgumentException("word length is not 4");
+		}
+		
+		this.words.set(row, word);
+			
+		}
+
+	
+	public String getRow(int row) {
+		
+		if (row < 0 || row > 3) {
+			throw new IllegalArgumentException("row index not valid");
+		}
+		
+		return this.words.get(row);
+	}
+	
+	
+	public String getColumn(int col) {
+		
+		if (col < 0 || col > 3) {
+			throw new IllegalArgumentException("row index not valid");
+		}
+		
+		String temp = "";
+		
+		for (int i = 0; i < 4; i++) {
+			temp = temp + this.words.get(i).charAt(col);
+		}
+		
+		return temp;
+	}
+	
+	
+	public boolean isSymmetric() {
+			
+		for (int i = 0; i < 4; i++) {
+			if (!(this.getRow(i).equals(this.getColumn(i)))) {
+				return false;
+			}
+		}
+		
+		return true;
+		}
+	
+	
+	public boolean isComplete() {
+		
+		boolean temp = true;
+		
+		if (this.isSymmetric()) {
+			for (int i = 0; i < 4; i++) {
+				if (!(dict.contains(this.words.get(i)))){
+					temp = false;
+				}
+			}
+		}
+		else {
+			temp = false;
+		}
+		
+		return temp;
+	}
+	
+	
+	public static SymCross makeCrossword(String first) {
+		
+		SymCross puzzle = new SymCross();
+		
+		if (dict.contains(first)) {
+			
+			puzzle.setWord(0, first);
+			
+			//ArrayList<String> col1 = dict.startsWith(puzzle.getColumn(0).substring(0,1));
+			ArrayList<String> col2 = dict.startsWith("" + first.charAt(1));
+			ArrayList<String> col3 = dict.startsWith("" + first.charAt(2));
+			ArrayList<String> col4 = dict.startsWith("" + first.charAt(3));
+			
+			for (String c1 : col2) {
+				puzzle.setWord(1, c1);
+				for (String c2 : col3) {
+					puzzle.setWord(2, c2);
+					for (String c3 : col4) {
+						puzzle.setWord(3, c3);
+						if (puzzle.isSymmetric()) {
+							return puzzle;
+						}
+					}
+				}
+			}
+		}
+		
+		return puzzle;
+	}
+		
+	
+	
+	public String toString() {
+		
+		String puzzle = "";
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				puzzle = puzzle + this.words.get(i).charAt(j) + " ";
+			}
+
+			puzzle = puzzle + "\n";
+		}
+		
+		puzzle = puzzle.substring(0, puzzle.length() - 1);
+		return puzzle;
+	}
+	
+	
+	/**
+	 * For testing purposes. Students are encouraged to modify this method to
+	 * further test their code.
+	 * 
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+		String first = "COST";
+		SymCross cw = SymCross.makeCrossword(first);
+		if (cw.isComplete()) {
+			for (int i = 0; i < 4; i++) {
+				System.out.println(cw.getRow(i));
+			}
+		} else {
+			System.out.println("no crossword starting with " + first + " exists");
+		}
+		
+		System.out.println();
+		System.out.println(cw.toString());
+
+	}
+
+}
---
+++ src/Project/dictionary/Dictionary.java
@@ -0,0 +1,182 @@
+package a3.dictionary;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+/**
+ * A simple dictionary of 4-letter English words that appear in the official
+ * list of Scrabble words. This dictionary allows the user to test if a word
+ * exists in the dictionary and allows the user to retrieve a list of all of the
+ * words that start with a given string (for example, find all of the words that
+ * start with "TH").
+ *
+ */
+public class Dictionary {
+
+	/**
+	 * The list of words in this dictionary in sorted alphabetic order.
+	 */
+	private ArrayList<String> words;
+
+	/**
+	 * The list of 26 indexes in this.words for the first word starting with A, B,
+	 * C, ... That is:
+	 * 
+	 * indexOfLetter.get(0) is the index of the first word starting with A
+	 * indexOfLetter.get(1) is the index of the first word starting with B
+	 * indexOfLetter.get(2) is the index of the first word starting with C and so on
+	 */
+	private ArrayList<Integer> indexOfLetter;
+
+	/**
+	 * Initializes this dictionary according to the description in the assignment
+	 * document.
+	 */
+	public Dictionary() {
+		// Do not change the next two lines...
+		this.words = new ArrayList<>();
+		this.readFile();
+		
+		System.out.println(words);
+		//System.out.println(words.size());
+
+		// Complete this constructor...
+		this.indexOfLetter = new ArrayList<Integer>(26); // create a list of 26 entries
+		
+		for (int i = 0; i < 26; i ++) {
+			this.indexOfLetter.add(-1); //fill the list with default -1 values
+		}
+		
+		for (int i = 0; i < words.size(); i++) {
+			char firstLetterOfWord = words.get(i).charAt(0);
+			int letterIndex = firstLetterOfWord - 'A';
+			
+			if (indexOfLetter.get(letterIndex) == -1) {
+				indexOfLetter.set(letterIndex, i);
+			}
+		}
+	}
+
+	/**
+	 * Reads the words of this dictionary from the file found in words/four.txt
+	 * 
+	 * <p>
+	 * The words in the file are already in sorted alphabetic order.
+	 * 
+	 * <p>
+	 * ALREADY IMPLEMENTED FOR YOU.
+	 */
+	void readFile() {
+		Path root = FileSystems.getDefault().getPath("").toAbsolutePath();
+		Path filePath = Paths.get(root.toString(), "words", "four.txt");
+		String path = filePath.toString();
+		
+		try (Stream<String> stream = Files.lines(Paths.get(path))) {
+			stream.forEach(this.words::add);
+		} 
+		catch (IOException ex) {
+			throw new RuntimeException("error reading dictionary file");
+		}
+	}
+
+	
+	/*
+	 * Add the required methods after this comment.
+	 */
+	
+	public boolean contains(String word) {
+		// the case of word is ignored when searching this dictionary
+		
+		word = word.toUpperCase();
+		
+		for (int i = 0; i < this.words.size(); i++) {
+			if (this.words.get(i).equals(word)) {
+				return true;
+			}	
+		}
+		
+		return false;
+
+	}
+	
+	private int indexOf(int c) {
+		
+		if (c < 'A') {
+			return 0;
+		}
+		
+		if (c > 'Z') {
+			return this.words.size();
+		}
+		
+		int letterIndex = c - 'A';
+		
+		return this.indexOfLetter.get(letterIndex);
+		
+	}
+	
+	public ArrayList<String> startsWith(String prefix){
+		
+		ArrayList<String> wordList = new ArrayList<>();
+		
+		char firstChar = prefix.charAt(0);
+		int letterIndex = this.indexOf(firstChar);
+		
+		if (prefix.equals("")) {
+			wordList.addAll(this.words);
+		}
+		else if (prefix.length() > 4){
+			wordList.clear();
+		}
+		else {
+			for (int i = letterIndex; i < this.indexOf(firstChar + 1); i++){
+				
+				boolean tempBoo = true;
+				String tempString = this.words.get(i);
+				
+				for (int j = 0; j < prefix.length(); j++) {
+					if (tempString.charAt(j) != prefix.charAt(j)) {
+						tempBoo = false;
+					}
+				}
+				
+				if (tempBoo){
+					wordList.add(this.words.get(i));
+				}
+			}
+		}
+		
+		return wordList;
+	}
+	
+
+	/**
+	 * For testing purposes. Students are encouraged to modify this method to
+	 * further test their code.
+	 * 
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+		Dictionary d = new Dictionary();
+		System.out.println("Contains \"SORT\"? " + d.contains("SORT"));
+		System.out.println("Contains \"EROS\"? " + d.contains("EROS"));
+		System.out.println("Contains \"AH\"? " + d.contains("AH"));
+		System.out.println("Contains \"AAH\"? " + d.contains("AAH"));
+		System.out.println("Contains \"YODE\"? " + d.contains("YODE"));
+		System.out.println("Contains \"ZYME\"? " + d.contains("ZYME"));
+		System.out.println("Contains \"ZYMM\"? " + d.contains("ZYMM"));
+		System.out.println("Words starting with \"AB\"? " + d.startsWith("AB"));
+		System.out.println("Words starting with \"DE\"? " + d.startsWith("DE"));
+		System.out.println("Words starting with \"GECEYI\"? " + d.startsWith("GECEYI"));
+		System.out.println("Words starting with \"AAAA\"? " + d.startsWith("AAAA"));
+		System.out.println("Index of \"A\"? " + d.indexOf('A'));
+		System.out.println("Index of \"B\"? " + d.indexOf('B'));
+		System.out.println("Index of \"J\"? " + d.indexOf('J'));
+		System.out.println("Index of \"Z\"? " + d.indexOf('Z'));
+	}
+}
